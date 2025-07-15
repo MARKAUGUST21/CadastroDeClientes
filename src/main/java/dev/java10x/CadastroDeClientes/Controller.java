bad6c7ffdc0e988e0e5089c122f2ed6938b8de84
Marcos Pedro Camargo <mcmpgui12@gmail.com>
2025-07-15 20:10:31 -0300

    Criado a Controller aula02

---
+++ src/main/java/dev/java10x/CadastroDeClientes/Controller.java
@@ -0,0 +1,22 @@
+package dev.java10x.CadastroDeClientes;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Essa é a primeira mensagem nessa rota";
+
+    }
+
+
+
+}
