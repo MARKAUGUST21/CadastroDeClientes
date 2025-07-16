9850b5e159f5db8e0f96a4ab2a1b9f62a29b90ea
Marcos Pedro Camargo <mcmpgui12@gmail.com>
2025-07-15 21:40:41 -0300

    Acrescentando private

--- src/main/java/dev/java10x/CadastroDeClientes/ClienteModel.java
+++ src/main/java/dev/java10x/CadastroDeClientes/ClienteModel.java
@@ -1,4 +1,61 @@
 package dev.java10x.CadastroDeClientes;
 
+import jakarta.persistence.*;
+
+
+// Entity transforma uma classe em uma entidade do BD
+@Entity
+@Table(name = "tb_cadastro")
 public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cidade;
+    private String profissão;
+    private String escolaridade;
+
+    public ClienteModel(){
+
+    }
+
+    public ClienteModel(String nome, String cidade, String profissão, String escolaridade) {
+        this.nome = nome;
+        this.cidade = cidade;
+        this.profissão = profissão;
+        this.escolaridade = escolaridade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getProfissão() {
+        return profissão;
+    }
+
+    public void setProfissão(String profissão) {
+        this.profissão = profissão;
+    }
+
+    public String getEscolaridade() {
+        return escolaridade;
+    }
+
+    public void setEscolaridade(String escolaridade) {
+        this.escolaridade = escolaridade;
+    }
 }
